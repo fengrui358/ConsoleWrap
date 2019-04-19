7cfc29c579fc2eca2392a4704ce52f6b59aa888a
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2019-04-19 18:18:43 +0800

    提交

---
+++ ConsoleWrap/IOptions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace ConsoleWrap
+{
+    interface IOptions
+    {
+        [Option('f', "file",
+            HelpText = "Input file to be execute.",
+            Required = true)]
+        string FilePath { get; set; }
+
+        [Option('t', "title",
+            Default = "Program execute",
+            HelpText = "Describe email title")]
+        string Title { get; set; }
+    }
+
+    /// <summary>
+    /// 子命令需要加入Verb，如果是单独的命令可不需要
+    /// </summary>
+    class HeadOptions : IOptions
+    {
+        public string FilePath { get; set; }
+        public string Title { get; set; }
+
+        [Usage(ApplicationAlias = "ReadText.Demo.exe")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("normal scenario", new HeadOptions { FilePath = "file.exe", Title = "执行 file.exe"});
+            }
+        }
+    }
+}
---
+++ ConsoleWrap/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CommandLine;
+using NLog;
+
+namespace ConsoleWrap
+{
+    /// <summary>
+    /// 控制台程序的基础框架
+    /// </summary>
+    class Program
+    {
+        private static string _filePath;
+        private static string _title;
+
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        static async Task Main(string[] args)
+        {
+            //处理参数
+            void Header(IOptions opts)
+            {
+                if (!string.IsNullOrEmpty(opts.FilePath))
+                {
+                    if (File.Exists(opts.FilePath))
+                    {
+                        _filePath = opts.FilePath;
+                    }
+                    else
+                    {
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory(), opts.FilePath);
+                        if (File.Exists(opts.FilePath))
+                        {
+                            _filePath = opts.FilePath;
+                        }
+                    }
+                }
+
+                _title = opts.Title;
+            }
+
+            var result = Parser.Default.ParseArguments<HeadOptions>(args);
+            result.WithParsed(Header);
+
+            if (_filePath != null && File.Exists(_filePath))
+            {
+                try
+                {
+                    //清理目录下的原有日志
+                    var directory = new FileInfo(_filePath).Directory;
+                    var logs = directory?.GetFiles("natapp.log*", SearchOption.TopDirectoryOnly);
+
+                    foreach (var fileInfo in logs)
+                    {
+                        fileInfo.Delete();
+                    }
+
+                    var sb = new StringBuilder();
+
+                    RunProcess(_filePath);
+
+                    sb.AppendLine(_title);
+                    sb.AppendLine($"执行{Path.GetFileName(_filePath)}");
+                    //sb.AppendLine(s);
+
+                    //延迟5秒读取日志
+                    await Task.Delay(5000);
+
+                    var log = directory?.GetFiles("natapp.log*", SearchOption.TopDirectoryOnly).FirstOrDefault();
+                    if (log == null)
+                    {
+                        throw new Exception("读取日志失败");
+                    }
+                    else
+                    {
+                        var regex = new Regex("\"Url\":\"(.+?)\"");
+
+                        var fs = new FileStream(log.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                        using (var sr = new StreamReader(fs))
+                        {
+                            var content = sr.ReadToEnd();
+
+                            var match = regex.Match(content);
+                            if (match.Success && match.Groups.Count == 2)
+                            {
+                                var url = match.Groups[1].Value;
+                                sb.AppendLine(url);
+                            }
+                        }
+                    }
+
+                    Logger.Info(sb.ToString);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Logger.Error(e);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"文件不存在:{_filePath}");
+            }
+        }
+
+        /// <summary>
+        /// 获取Cmd进程
+        /// </summary>
+        /// <returns></returns>
+        private static Process RunProcess(string filePath)
+        {
+            var proc = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "natapp.exe",
+                    UseShellExecute = false,
+                    //RedirectStandardInput = true,
+                    //RedirectStandardError = true,
+                    //RedirectStandardOutput = true,
+                    CreateNoWindow = false
+                }
+            };
+
+            proc.Start();
+            return proc;
+        }
+    }
+}
